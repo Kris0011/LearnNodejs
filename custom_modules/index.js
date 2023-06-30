16fdf2ab5f49b9e80d6c4ac415ef6db1eb069679
Kris Patel <krishp759@gmail.com>
2023-06-30 22:59:12 +0530

    Custom module

---
+++ custom_modules/index.js
@@ -0,0 +1,6 @@
+console.log('this is index')
+const mod = require('./module');
+
+console.log(mod.sum(1,2));
+console.log(mod.name);
+console.log(mod.repo);
---
+++ custom_modules/module.js
@@ -0,0 +1,10 @@
+console.log('this is module');
+
+function sum(a,b){
+    return a+b;
+}
+module.exports= {
+    sum : sum ,
+    name : "Kris" ,
+    repo : "LearnNodejs"
+}
